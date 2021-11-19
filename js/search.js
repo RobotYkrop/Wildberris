b59000a389f71ca89ecb6388a0e900435dc28856
RobotYkrop <dendozzor@gmail.com>
2021-11-19 15:21:45 +0500

    Commit

---
+++ js/search.js
@@ -0,0 +1,10 @@
+const search = function () {
+    const input = document.querySelector('.seacrh-block > input')
+    const searchBtn = document.querySelector('.seacrh-block > button')
+
+    input.addEventListener('input' , (event) => {
+        
+    })
+}
+
+search()
